5a45785685ce34548f756a902c09e08214559158
Nass1m <70798807+Nass1m@users.noreply.github.com>
2023-05-18 21:22:11 +0200

    Soap 13 (#3)
    * werkt niet
    
    * SOAP-13 happyflow test
    
    * fix: removed credentials
    
    * SOAP-13 laatste API tests
    
    * SOAP-13 werkende SOAP error
    
    ---------
    
    Co-authored-by: JBeaart <jbtwaalf@gmail.com>

--- WebShopApiTests/GetPaymentMethodCollection.cs
+++ WebShopApiTests/GetPaymentMethodCollection.cs
@@ -2,6 +2,9 @@ namespace WebShopApiTests;
 using NUnit.Framework;
 using System;
 using ServiceReference;
+using System.ServiceModel;
+using System.Text;
+using System.Net;
 
 public class GetPaymentMethodCollection
 {
@@ -41,6 +44,59 @@ public class GetPaymentMethodCollection
       Assert.IsTrue(ValidPaymentMethods.Contains(paymentMethod.Name), $"Unexpected payment method found: {paymentMethod.Name}");
     }
   }
+
+  [Test]
+  public async Task TestInvalidUsername()
+  {
+    // Arrange
+    string invalidUsername = "incorrect_username";
+
+    // Act & Assert
+    var exception = Assert.ThrowsAsync<FaultException<ExceptionDetail>>(async () => await _client.GetPaymentMethodCollectionAsync(invalidUsername, password));
+    StringAssert.Contains("Not allowed", exception?.Message);
+
+  }
+
+  [Test]
+  public async Task TestInvalidPassword()
+  {
+    // Arrange
+    string invalidPassword = "incorrect_password";
+
+    // Act & Assert
+    var exception = Assert.ThrowsAsync<FaultException<ExceptionDetail>>(async () => await _client.GetPaymentMethodCollectionAsync(username, invalidPassword));
+    StringAssert.Contains("Not allowed", exception?.Message);
+
+  }
+
+  [Test]
+  public async Task TestInvalidSoapRequest()
+  {
+    string soapRequest = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:web=""http://tempuri.org/"">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <web:GetPaymentMethodCollection>
+         <web:usernameOrEmail>test@test.com</web:usernameOrEmil>
+         <web:userPassword>password</web:userPassword>
+      </web:GetPaymentMethodCollection>
+   </soapenv:Body>
+</soapenv:Envelope>";
+
+    HttpClient client = new HttpClient();
+    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://demowebshop.tricentis.com/Plugins/Misc.WebServicesCustomer/Remote/NopService.svc");
+    request.Content = new StringContent(soapRequest, Encoding.UTF8, "text/xml");
+    request.Headers.Add("SOAPAction", "http://tempuri.org/INopService/GetPaymentMethodCollection");
+
+    // Act
+    HttpResponseMessage response = await client.SendAsync(request);
+    string responseContent = await response.Content.ReadAsStringAsync();
+
+    // Assert
+    StringAssert.Contains("an error deserializing the object", responseContent);
+    Assert.AreEqual(HttpStatusCode.InternalServerError, response.StatusCode);
+  }
+
+
 
 }
 
